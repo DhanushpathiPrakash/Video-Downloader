9dab5b631906abc9058a77919f2d5942ff7b5a4f
DhanushpathiPrakash <dhanushpathiprakash@gmail.com>
2022-02-18 22:32:04 +0530

    Add

--- run.py
+++ run.py
@@ -3,19 +3,3 @@ from video import app
 if __name__ == '__main__':
     app.run(debug=True)
 
-'''
-pip install email_validator
-pip install flask_sqlalchemy
-pip install flask_wtf
-pip install flask_login
-pip install flask_bcrypt
-
-'''
-'''
-from video import db
-db.create_all()
-from video.models import Admin
-ad = Admin(email="admin@admin.com", password="admin")
-db.session.add(ad)
-db.session.commit()
-'''
